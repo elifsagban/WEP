d3eb128b8302b1f4fe620ff94bba261aaec756f7
elifsagban <elifsagban@gmail.com>
2021-02-22 00:11:50 +0300

    Firebase + register

--- app/src/main/java/com/elif/wep/MainActivity.java
+++ app/src/main/java/com/elif/wep/MainActivity.java
@@ -2,13 +2,31 @@ package com.elif.wep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        regButton = (Button) findViewById(R.id.mainRegister);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegisterPage();
+            }
+        });
+
+    }
+
+    private void openRegisterPage() {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/elif/wep/Register.java
+++ app/src/main/java/com/elif/wep/Register.java
@@ -1,14 +1,61 @@
 package com.elif.wep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText nameText, emailText, passwordText;
+    Button registerBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        nameText = findViewById(R.id.name);
+        emailText = findViewById(R.id.email);
+        passwordText = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameText.getText().toString();
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(name) || TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this, "Fill out each cell!", Toast.LENGTH_SHORT).show();
+                }else if (password.length() < 6){
+                    Toast.makeText(Register.this, "Please enter a password more than 6 characters", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(Register.this, "Registered successfully!", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(Register.this, "The user is not registered!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
