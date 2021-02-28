bb5fcf43193d6eae5c1185ee531bc2c5b4f9be23
elifsagban <elifsagban@gmail.com>
2021-02-28 12:42:41 +0300

    bottom navbar

--- app/src/main/java/com/elif/wep/MainActivity.java
+++ app/src/main/java/com/elif/wep/MainActivity.java
@@ -6,39 +6,102 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button regButton;
-    private Button taskButton;
+    private ImageButton statPage;
+    private ImageButton planPage;
+    private ImageButton homePage;
+    private ImageButton taskPage;
+    private ImageButton profilePage;
 
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        regButton = (Button) findViewById(R.id.mainRegister);
-        regButton.setOnClickListener(new View.OnClickListener() {
+        registerBtn = (Button) findViewById(R.id.mainRegister);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
                 openRegisterPage();
+
             }
         });
-
-        taskButton = findViewById(R.id.mainTask);
-        taskButton.setOnClickListener(new View.OnClickListener() {
+        statPage = (ImageButton) findViewById(R.id.statPage);
+        statPage.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
-                openTaskPage();
+            public void onClick(View v) {
+
+                openStatsPage();
+
             }
         });
+        planPage = (ImageButton) findViewById(R.id.planPage);
+        planPage.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
 
+                openPlanPage();
+
+            }
+        });
+        homePage = (ImageButton) findViewById(R.id.homePage);
+        homePage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openHomePage();
+
+            }
+        });
+        taskPage = (ImageButton) findViewById(R.id.taskPage);
+        taskPage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openTaskPage();
+
+            }
+        });
+        profilePage = (ImageButton) findViewById(R.id.profilePage);
+        profilePage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openProfilePage();
+
+            }
+        });
     }
-
+    public void openStatsPage() {
+        Intent intent = new Intent(this, Statistics.class);
+        startActivity(intent);
+    }
+    private void openPlanPage() {
+        Intent intent = new Intent(this, Schedule.class);
+        startActivity(intent);
+    }
+    private void openHomePage() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
     private void openTaskPage() {
         Intent intent = new Intent(this, TaskList.class);
+        startActivity(intent);
+    }
+    private void openProfilePage() {
+        Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/elif/wep/Statistics.java
+++ app/src/main/java/com/elif/wep/Statistics.java
@@ -2,13 +2,96 @@ package com.elif.wep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Statistics extends AppCompatActivity {
+    private ImageButton statPage;
+    private ImageButton planPage;
+    private ImageButton homePage;
+    private ImageButton taskPage;
+    private ImageButton profilePage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        statPage = (ImageButton) findViewById(R.id.statPage);
+        statPage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openStatsPage();
+
+            }
+        });
+        planPage = (ImageButton) findViewById(R.id.planPage);
+        planPage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openPlanPage();
+
+            }
+        });
+        homePage = (ImageButton) findViewById(R.id.homePage);
+        homePage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openHomePage();
+
+            }
+        });
+        taskPage = (ImageButton) findViewById(R.id.taskPage);
+        taskPage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openTaskPage();
+
+            }
+        });
+        profilePage = (ImageButton) findViewById(R.id.profilePage);
+        profilePage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openProfilePage();
+
+            }
+        });
     }
+    public void openStatsPage() {
+        Intent intent = new Intent(this, Statistics.class);
+        startActivity(intent);
+    }
+    private void openPlanPage() {
+        Intent intent = new Intent(this, Schedule.class);
+        startActivity(intent);
+    }
+    private void openHomePage() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    private void openTaskPage() {
+        Intent intent = new Intent(this, TaskList.class);
+        startActivity(intent);
+    }
+    private void openProfilePage() {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+
+
 }
