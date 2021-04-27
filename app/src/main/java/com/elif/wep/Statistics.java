28bf98faab8e1a98144631da1a85821debeee284
elifsagban <elifsagban@gmail.com>
2021-04-27 22:45:25 +0300

    bottom menu nav

--- app/src/main/java/com/elif/wep/Statistics.java
+++ app/src/main/java/com/elif/wep/Statistics.java
@@ -1,13 +1,17 @@
 package com.elif.wep;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,7 +32,37 @@ public class Statistics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.stat_item);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            private MenuItem item;
 
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                this.item = item;
+                switch (item.getItemId()){
+                    case R.id.home_item:
+                        startActivity(new Intent(getApplicationContext()
+                                , MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.task_item:
+                        startActivity(new Intent(getApplicationContext()
+                                , TaskList.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.plan_item:
+                        startActivity(new Intent(getApplicationContext()
+                                , Schedule.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.stat_item:
+                        return true;
+
+                }
+                return false;
+            }
+        });
         fAuth = FirebaseAuth.getInstance();
         userID = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
         db = FirebaseDatabase.getInstance().getReference().child("tasks").child(userID);
